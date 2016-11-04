fd9ca7a267fd078bf1d7061496d7bb55185555bb
mberkman <mberkman@anonimatus.com>
2016-11-04 10:22:10 -0300

    Configs files

---
+++ classes/db_connection.php
@@ -0,0 +1,11 @@
+<?php 
+class Data_Base_Conn{
+      protected $server = "mysql.hostinger.com.ar"; 
+      protected $server_user = "u835328347_root"; 
+      protected $server_password = "123456"; 
+      protected $DB_NAME = "u835328347_db"; 
+
+      protected $DB_CONNECTION;
+      protected $conn;
+}
+?>
---
+++ hanala/config.php
@@ -0,0 +1,5 @@
+<?php 
+$BASE_URL = "http://orjadashmajane.com/hanala/";
+$BASE_FRONT_URL = "http://orjadashmajane.com/";
+$dirpath = realpath(dirname(getcwd()))."/";
+?>
